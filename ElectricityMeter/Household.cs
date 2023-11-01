1f9b70371bbdcfb7091dd79494de81ccde7444fb
Adam46072 <adam.bartl2001@seznam.cz>
2023-11-01 17:39:00 +0100

    household almost done

--- ElectricityMeter/Household.cs
+++ ElectricityMeter/Household.cs
@@ -8,5 +8,35 @@ namespace ElectricityMeter
 {
     internal class Household
     {
+        public string adressHome { get; private set; }
+        public IPowerMeter powerMeter { get; private set; }
+        public Household(string adressHome)
+        {
+            this.adressHome = adressHome;
+        }
+        public void InstallPowerMeter(IPowerMeter powerMeter)
+        {
+            this.powerMeter = powerMeter;
+        }
+        public void UseElectricity()
+        {
+            Thread t1 = new Thread(powerMeter.Consumption);
+            char e;
+            do
+            {
+                e = Console.ReadKey().KeyChar;
+                if (e.Equals('e'))
+                {
+                    powerMeter.TurnOn(true);
+                    t1.Start();
+                }
+                else
+                {
+                    powerMeter.TurnOn(false);
+                }
+
+            } while (e == 'e');
+
+        }
     }
 }

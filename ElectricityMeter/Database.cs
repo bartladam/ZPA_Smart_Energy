4d43ff176d3b9d7cd698a0b73ca8e5b65ff7ed01
Adam46072 <adam.bartl2001@seznam.cz>
2023-11-01 18:08:44 +0100

    Database is connected with Household

--- ElectricityMeter/Database.cs
+++ ElectricityMeter/Database.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,24 @@ namespace ElectricityMeter
 {
     internal class Database
     {
+        private List<string> information { get; set; }
+        public Database()
+        {
+            information = new List<string>();
+        }
+        public void AddInformation(string text, DateTime dt, float count)
+        {
+            information.Add(string.Format("{0}\n{1}\n{2}\n", text, dt.ToString(), count.ToString()));
+        }
+        public override string ToString()
+        {
+            string allDats = "";
+            foreach (string item in information)
+            {
+                allDats += item;
+            }
+            return allDats;
+        }
+
     }
 }
